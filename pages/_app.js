763e71361a7859b99512b0dc82b6499330701ab1
John Butler <johnmichaelbutler@yahoo.com>
2021-03-25 11:43:56 -0500

    Added Head element to _app.js to include icon for browser tab.

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import '../styles/globals.css';
 import MyLayout from '../layouts/MyLayout';
+import Head from 'next/head';
 import Seo from '../next-seo.config';
 
 function MyApp({ Component, pageProps }) {
   return (
     <MyLayout>
+      <Head>
+        <link rel="icon" href="/logo_white.ico" />
+      </Head>
       <Seo />
       <Component {...pageProps} />
     </MyLayout>
